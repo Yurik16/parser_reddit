e2ff6317ffa54ed811e24a529655257bcf771b3a
YurySE <chuksin.yury@gmail.com>
2021-11-10 06:19:14 +0300

    Try get response from json request
    get better

--- parser.py
+++ parser.py
@@ -69,37 +69,38 @@ def drv_parse():
     """Make parsing with Chrome"""
     driver.get(url=URL)
     time.sleep(5)
-    for _ in range(2):
+    for _ in range(12):
         elements = driver.find_elements(By.CLASS_NAME, '_1oQyIsiPHYt6nx7VOmd1sz')
         print("Results count: %d" % len(elements))
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         time.sleep(3)
     drv_html = driver.page_source
     get_content(drv_html)
-    ull = get_users_links_list(my_lib)
-    pprint.pprint(ull)
+    # ull = get_users_links_list(my_lib)
+    # pprint.pprint(ull)
 
 
 def parse_user_list(lib: list):
     """Make a data dict out of users page"""
-    users_page = {}
+    users_page = []
     for el in lib:
+        print(el)
         response = requests.get(el)
-        if response.status_code == 200:
-            html = response.content
-            soup = BeautifulSoup(html, 'html.parser')
-            user_div = soup.find('div', class_='_3Im6OD67aKo33nql4FpSp_')
-            try:
-                users_page[f'{el[28:-1]} user'] = \
-                    user_div.find('span', class_='_28nEhn86_R1ENZ59eAru8S').text.split(' ')[0][2::]
-                users_page[f'{el[28:-1]} karma'] = user_div.find('span',
-                                                                 id='profile--id-card--highlight-tooltip--karma').text
-                # user_page["cake day"] = user_div.find_next('span', class_='_3KNaG9-PoXf4gcuy5_RCVy').text
-            except AttributeError:
-                print("Data error")
-        else:
-            print("Please check URL or HEADERS")
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        user_div = soup.find('div', class_='_3Im6OD67aKo33nql4FpSp_')
+        try:
+            users_page.append({
+                "username": f'{el[28:-1]}',
+                # "user": user_div.find('span', class_='_28nEhn86_R1ENZ59eAru8S').text,
+                "karma": user_div.find('span', id='profile--id-card--highlight-tooltip--karma').text,
+                "cake day": user_div.find_next('span', id='profile--id-card--highlight-tooltip--cakeday').text,
+            })
+        except AttributeError:
+            print("Data error")
+
     pprint.pprint(users_page)
+    print(len(users_page))
 
 
 def get_users_data_from_json(lib: list):
@@ -119,8 +120,11 @@ def get_users_data_from_json(lib: list):
     return users_data
 
 
+# drv_parse()
+# users_name_plus = get_users_name(my_lib)
+# ull = get_users_links_list(my_lib)
+# users_data_list = get_users_data_from_json(users_name_plus)
+# pprint.pprint(users_data_list)
 drv_parse()
-users_name_plus = get_users_name(my_lib)
 ull = get_users_links_list(my_lib)
-users_data_list = get_users_data_from_json(users_name_plus)
-pprint.pprint(users_data_list)
+parse_user_list(ull)

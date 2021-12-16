70ee04175181cf9a75afe3943bb9d7c9e01df9de
YurySE <chuksin.yury@gmail.com>
2021-12-16 20:07:40 +0300

    Create post_table init. Fix insert_user def by restricting duplicates

--- pg_users.py
+++ pg_users.py
@@ -26,30 +26,57 @@ def create_table_users():
         cake_day DATE NOT NULL);
         """
     conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
+    params = config()
+    conn = psycopg2.connect(**params)
+    with conn:
         cur = conn.cursor()
-        cur.execute(command)
-        cur.close()
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+        try:
+            cur.execute(command)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+
+def create_table_posts():
+    """Create table pg_posts in the PostgreSQL database"""
+    command = """
+    CREATE TABLE pg_posts (
+        post_id INTEGER PRIMARY KEY,
+        post_category VARCHAR(50) NOT NULL,
+        post_link VARCHAR NOT NULL,
+        number_of_comments INTEGER NOT NULL,
+        post_votes INTEGER NOT NULL,
+        post_date DATE NOT NULL);
+        FOREIGN KEY (post_id) 
+            REFERENCES pg_users (user_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+        """
+    conn = None
+    params = config()
+    conn = psycopg2.connect(**params)
+    with conn:
+        cur = conn.cursor()
+        try:
+            cur.execute(command)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
 
 def insert_user(user_name: str, total_carma: int, comment_carma: int, cake_day: datetime):
     """Insertion a new user into users table"""
     cmd = "INSERT INTO pg_users (user_name, total_carma, comment_carma, cake_day) VALUES (%s, %s, %s, %s)"
+    is_duplicate = "SELECT count(*) FROM pg_users WHERE user_name LIKE %s"
 
     params = config()
     connection = psycopg2.connect(**params)
     with connection:
         cursor = connection.cursor()
+        cursor.execute(is_duplicate, (user_name, ))
+        result = cursor.fetchone()
+        if result[0]:
+            print("duplicates detected")
+            return
         cursor.execute(cmd, (user_name, total_carma, comment_carma, cake_day))
 
 
 create_table_users()
-insert_user('first_user', 1000, 10, datetime(12, 12, 12))
+insert_user('second_user', 2000, 20, datetime(2021, 12, 12))

64091f9e5ddd3acd8d88c5588eb31d9de668c87f
YurySE <chuksin.yury@gmail.com>
2021-12-17 03:22:49 +0300

    Create def insert_post. Change foreign key for pg_posts table

--- pg_users.py
+++ pg_users.py
@@ -40,13 +40,14 @@ def create_table_posts():
     """Create table pg_posts in the PostgreSQL database"""
     command = """
     CREATE TABLE pg_posts (
-        post_id INTEGER PRIMARY KEY,
+        post_id SERIAL PRIMARY KEY,
+        author INTEGER NOT NULL,
         post_category VARCHAR(50) NOT NULL,
         post_link VARCHAR NOT NULL,
         number_of_comments INTEGER NOT NULL,
         post_votes INTEGER NOT NULL,
         post_date DATE NOT NULL,
-        FOREIGN KEY (post_id) 
+        FOREIGN KEY (author) 
             REFERENCES pg_users (user_id)
             ON UPDATE CASCADE ON DELETE CASCADE
             )
@@ -79,6 +80,31 @@ def insert_user(user_name: str, total_carma: int, comment_carma: int, cake_day: 
         cursor.execute(cmd, (user_name, total_carma, comment_carma, cake_day))
 
 
+def insert_post(author: str, post_category: str, post_link: str, number_of_comments: int, post_votes: int, post_date: datetime):
+    """Insertion a new post into users table"""
+    cmd = "INSERT INTO pg_posts (" \
+          "author, post_category, post_link, number_of_comments, post_votes, post_date" \
+          ") VALUES (%s, %s, %s, %s, %s, %s)"
+    is_duplicate = "SELECT count(*) FROM pg_posts WHERE post_link LIKE %s"
+    user_id_is = "SELECT user_id FROM pg_users WHERE user_name LIKE %s"
+
+    params = config()
+    connection = psycopg2.connect(**params)
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(is_duplicate, (post_link,))
+        count_of_duplicate = cursor.fetchone()
+        if count_of_duplicate[0]:
+            print("duplicates detected")
+            return
+        cursor.execute(user_id_is, (author,))
+        user_id = cursor.fetchone()[0]
+        cursor.execute(cmd, (user_id, post_category, post_link, number_of_comments, post_votes, post_date))
+
+
 create_table_posts()
 create_table_users()
 insert_user('second_user', 2000, 20, datetime(2021, 12, 12))
+insert_post("second_user", "post_category", "post_link1", 22, 33, datetime.now())
+insert_post("second_user", "category", "post_link3", 22, 33, datetime.now())
+insert_post("first_user", "post_category", "post_link2", 22, 33, datetime.now())

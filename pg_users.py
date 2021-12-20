7b4e60e51f2ca49932e35f52633ed628c9f29280
YurySE <chuksin.yury@gmail.com>
2021-12-20 22:09:06 +0300

    Create def get_all_entry
    Fix date format viewing at serialization

--- pg_users.py
+++ pg_users.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 import psycopg2
@@ -40,7 +41,7 @@ def create_table_posts():
     """Create table pg_posts in the PostgreSQL database"""
     command = """
     CREATE TABLE pg_posts (
-        post_id SERIAL PRIMARY KEY,
+        post_uid VARCHAR PRIMARY KEY,
         author INTEGER NOT NULL,
         post_category VARCHAR(50) NOT NULL,
         post_link VARCHAR NOT NULL,
@@ -64,7 +65,7 @@ def create_table_posts():
 
 
 def insert_user(user_name: str, total_carma: int, comment_carma: int, cake_day: datetime):
-    """Insertion a new user into users table"""
+    """Insertion a new user into pg_users table"""
     cmd = "INSERT INTO pg_users (user_name, total_carma, comment_carma, cake_day) VALUES (%s, %s, %s, %s)"
     is_duplicate = "SELECT count(*) FROM pg_users WHERE user_name LIKE %s"
 
@@ -80,11 +81,12 @@ def insert_user(user_name: str, total_carma: int, comment_carma: int, cake_day: 
         cursor.execute(cmd, (user_name, total_carma, comment_carma, cake_day))
 
 
-def insert_post(author: str, post_category: str, post_link: str, number_of_comments: int, post_votes: int, post_date: datetime):
-    """Insertion a new post into users table"""
+def insert_post(uid: str, author: str, post_category: str, post_link: str, number_of_comments: int, post_votes: int,
+                post_date: datetime):
+    """Insertion a new post into pg_posts table"""
     cmd = "INSERT INTO pg_posts (" \
-          "author, post_category, post_link, number_of_comments, post_votes, post_date" \
-          ") VALUES (%s, %s, %s, %s, %s, %s)"
+          "post_uid, author, post_category, post_link, number_of_comments, post_votes, post_date" \
+          ") VALUES (%s, %s, %s, %s, %s, %s, %s)"
     is_duplicate = "SELECT count(*) FROM pg_posts WHERE post_link LIKE %s"
     user_id_is = "SELECT user_id FROM pg_users WHERE user_name LIKE %s"
 
@@ -97,14 +99,48 @@ def insert_post(author: str, post_category: str, post_link: str, number_of_comme
         if count_of_duplicate[0]:
             print("duplicates detected")
             return
-        cursor.execute(user_id_is, (author,))
-        user_id = cursor.fetchone()[0]
-        cursor.execute(cmd, (user_id, post_category, post_link, number_of_comments, post_votes, post_date))
+        try:
+            cursor.execute(user_id_is, (author,))
+            user_id = cursor.fetchone()[0]
+            cursor.execute(cmd, (uid, user_id, post_category, post_link, number_of_comments, post_votes, post_date))
+        except TypeError as te:
+            print(f'There is no "{author}" user at db')
+        except Exception as e:
+            print(e)
+
+
+def get_all_entry() -> str:
+    """Get all data from db"""
+    cmd = "SELECT * FROM pg_posts as p JOIN pg_users as u" \
+          " ON p.author = u.user_id"
+    params = config()
+    connection = psycopg2.connect(**params)
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(cmd)
+        result = cursor.fetchall()
+        to_list = [[f'{elem}' for elem in entry] for entry in result]
+        return json.dumps(to_list)
+
+
+def get_one_entry(id: int) -> str:
+    cmd = "SELECT * FROM pg_posts as p JOIN pg_users as u" \
+          " ON p.author = u.user_id WHERE "
+    params = config()
+    connection = psycopg2.connect(**params)
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(cmd)
+        result = cursor.fetchall()
+        to_list = [[f'{elem}' for elem in entry] for entry in result]
+        return json.dumps(to_list)
 
 
 create_table_posts()
 create_table_users()
-insert_user('second_user', 2000, 20, datetime(2021, 12, 12))
-insert_post("second_user", "post_category", "post_link1", 22, 33, datetime.now())
-insert_post("second_user", "category", "post_link3", 22, 33, datetime.now())
-insert_post("first_user", "post_category", "post_link2", 22, 33, datetime.now())
+insert_user('second_user', 2000, 20, datetime(2021, 12, 12).strftime('%Y/%m/%d'))
+insert_user('new_user', 3000, 30, datetime(2020, 11, 11).strftime('%Y/%m/%d'))
+insert_post("df8aef88", "second_user", "post_category", "post_link1", 22, 33, datetime.now().strftime('%Y/%m/%d'))
+insert_post("df7aef87", "new_user", "category", "post_link4", 24, 32, datetime.now().strftime('%Y/%m/%d'))
+insert_post("df6aef86", "first_user", "post_category", "post_link2", 22, 33, datetime.now().strftime('%Y/%m/%d'))
+print(get_all_entry())

370d36c002195630b95be7b325642cce4789000d
YurySE <chuksin.yury@gmail.com>
2021-12-24 00:15:45 +0300

    Create def update_post and _user tables

--- db_postgre.py
+++ db_postgre.py
@@ -112,22 +112,31 @@ def insert_post(uid: str, author: str, post_category: str, post_link: str, numbe
 
 def get_all_entry() -> list:
     """Get all data from postgre DB"""
-    cmd = "SELECT * FROM pg_posts as p JOIN pg_users as u" \
-          " ON p.author = u.user_id"
+    create_view = "CREATE OR REPLACE VIEW view_all AS SELECT " \
+                  "p.post_uid, p.post_category, p.post_link, p.number_of_comments, p.post_votes, " \
+                  "p.post_date, u.user_name, u.total_carma, u.comment_carma, u.cake_day " \
+                  "FROM pg_posts as p JOIN pg_users as u " \
+                  "ON p.author = u.user_id"
+    cmd = "SELECT * FROM view_all"
 
     params = config()
     connection = psycopg2.connect(**params)
     with connection:
         cursor = connection.cursor()
+        cursor.execute(create_view)
         cursor.execute(cmd)
         result = cursor.fetchall()
         to_list = [[f'{elem}' for elem in entry] for entry in result]
         return to_list
 
 
-def get_one_entry(uid: str) -> str:
-    cmd = "SELECT * FROM pg_posts as p JOIN pg_users as u" \
-          " ON p.author = u.user_id WHERE p.post_uid LIKE %s"
+def get_one_entry(uid: str) -> list:
+    """Get certain data from postgre DB"""
+    cmd = "SELECT p.post_uid, p.post_category, p.post_link, p.number_of_comments, p.post_votes, " \
+          "p.post_date, u.user_name, u.total_carma, u.comment_carma, u.cake_day " \
+          "FROM pg_posts as p JOIN pg_users as u " \
+          "ON p.author = u.user_id " \
+          "WHERE p.post_uid LIKE %s"
     params = config()
     connection = psycopg2.connect(**params)
     with connection:
@@ -136,7 +145,7 @@ def get_one_entry(uid: str) -> str:
             cursor.execute(cmd, (uid,))
             result = cursor.fetchone()
             to_list = [f'{elem}' for elem in result]
-            return json.dumps(to_list)
+            return to_list
         except Exception as e:
             print(f'There is no such element in DB - {uid} ' + str(e))
 
@@ -155,8 +164,12 @@ def delete_post(uid: str):
 
 
 def update_post(uid: str, val: list):
-    """Update entry at postgre DB"""
-    cmd = "UPDATE pg_posts SET post_category = "
+    """Update post in pg_posts"""
+    i_gen = (item for item in val)
+    cmd = f"UPDATE pg_posts SET post_uid='{next(i_gen)}', author={next(i_gen)}, post_category='{next(i_gen)}', " \
+          f"post_link='{next(i_gen)}', number_of_comments={next(i_gen)}, " \
+          f"post_votes={next(i_gen)}, post_date='{next(i_gen)}'" \
+          f"WHERE post_uid LIKE %s"
     params = config()
     connection = psycopg2.connect(**params)
     with connection:
@@ -164,7 +177,23 @@ def update_post(uid: str, val: list):
         try:
             cursor.execute(cmd, (uid,))
         except Exception as e:
-            print(f'There is no such element in DB - {uid} ' + str(e))
+            print(f'There is no such post_uid in DB - {uid} ' + str(e))
+
+
+def update_user(user: str, val: list):
+    """Update user in gp_users"""
+    i_gen = (item for item in val)
+    cmd = f"UPDATE pg_users SET user_name='{next(i_gen)}', total_carma={next(i_gen)}, " \
+          f"comment_carma={next(i_gen)}, cake_day='{next(i_gen)}'" \
+          f"WHERE user_name LIKE %s"
+    params = config()
+    connection = psycopg2.connect(**params)
+    with connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute(cmd, (user,))
+        except Exception as e:
+            print(f'There is no such user_name in DB - {user} ' + str(e))
 
 
 create_table_posts()
@@ -177,6 +206,7 @@ insert_post("df7aef87", "new_user", "category", "post_link4", 24, 32, datetime.n
 insert_post("df6aef86", "first_user", "post_category", "post_link2", 22, 33, datetime.now().strftime('%Y/%m/%d'))
 
 print(get_all_entry())
-
-delete_post("df6aef86")
+new_entry = ['df6aef86', 3, 'category', 'link2', 111, 111, datetime.now().strftime('%Y/%m/%d')]
+# update_post('df6aef86', new_entry)
+# delete_post("df6aef86")
 print(get_one_entry("df6aef86"))

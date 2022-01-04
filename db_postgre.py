883797f02aebe8a3635b6fc45f02c091fa33a94c
YurySE <chuksin.yury@gmail.com>
2022-01-04 22:20:00 +0300

    Move creation of tables Users and Posts to __init__ via Update PostgreSQL
    Add docStrings
    Update PostgreSQL from 8.4 to 10+

--- db_postgre.py
+++ db_postgre.py
@@ -1,5 +1,7 @@
 import logging
 from configparser import ConfigParser
+from datetime import datetime
+
 import psycopg2
 
 
@@ -11,6 +13,39 @@ class PostgreDB:
         self.section = section
         self.parser = ConfigParser()
         self.parser.read(self.filename)
+        if self.parser.has_section(self.section):
+            self.params = self.parser.items(self.section)
+            for param in self.params:
+                self.db[param[0]] = param[1]
+        else:
+            raise Exception(f"Section {self.section} not found in the {self.filename} file")
+        self.connection = psycopg2.connect(**self.db)
+        self.cursor = self.connection.cursor()
+        self.connection.autocommit = True
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS pg_users(
+                user_id SERIAL PRIMARY KEY,
+                user_name VARCHAR(50) NOT NULL,
+                user_link VARCHAR(255) NOT NULL,
+                total_karma INTEGER NOT NULL,
+                comment_karma INTEGER NOT NULL,
+                link_karma INTEGER NOT NULL,
+                cake_day DATE NOT NULL);"""
+        )
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS pg_posts(
+                post_uid VARCHAR PRIMARY KEY,
+                author INTEGER NOT NULL,
+                post_category VARCHAR(50) NOT NULL,
+                post_date DATE NOT NULL,
+                number_of_comments VARCHAR(10) NOT NULL,
+                post_votes VARCHAR(10) NOT NULL,
+                post_link VARCHAR(255) NOT NULL,
+                FOREIGN KEY (author) 
+                    REFERENCES pg_users (user_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE
+                            );"""
+        )
         self.logging_init()
 
     def config(self) -> dict:
@@ -23,7 +58,8 @@ class PostgreDB:
             raise Exception(f"Section {self.section} not found in the {self.filename} file")
         return self.db
 
-    def logging_init(self) -> None:
+    @staticmethod
+    def logging_init() -> None:
         """Init logging"""
         logging.basicConfig(filename="log.txt", filemode='a',
                             format='%(asctime)s :: %(levelname)s :: %(funcName)s :: %(lineno)d :: %(message)s',
@@ -34,55 +70,18 @@ class PostgreDB:
         params = self.config()
         return psycopg2.connect(**params)
 
-    def create_table_users(self) -> None:
-        """Create table users in the PostgreSQL database"""
-        command = """
-        CREATE TABLE pg_users (
-            user_id SERIAL PRIMARY KEY,
-            user_name VARCHAR(50) NOT NULL,
-            user_link VARCHAR(255) NOT NULL,
-            total_karma INTEGER NOT NULL,
-            comment_karma INTEGER NOT NULL,
-            link_karma INTEGER NOT NULL,
-            cake_day DATE NOT NULL)
-            """
-        params = self.config()
-        connection = psycopg2.connect(**params)
-        with connection:
-            cursor = connection.cursor()
-            try:
-                cursor.execute(command)
-            except (Exception, psycopg2.DatabaseError) as error:
-                logging.warning(error)
-
-    def create_table_posts(self) -> None:
-        """Create table pg_posts in the PostgreSQL database"""
-        command = """
-        CREATE TABLE pg_posts (
-            post_uid VARCHAR PRIMARY KEY,
-            author INTEGER NOT NULL,
-            post_category VARCHAR(50) NOT NULL,
-            post_date DATE NOT NULL,
-            number_of_comments VARCHAR(10) NOT NULL,
-            post_votes VARCHAR(10) NOT NULL,
-            post_link VARCHAR(255) NOT NULL,
-            FOREIGN KEY (author) 
-                REFERENCES pg_users (user_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-                )
-            """
-        params = self.config()
-        connection = psycopg2.connect(**params)
-        with connection:
-            cursor = connection.cursor()
-            try:
-                cursor.execute(command)
-            except (Exception, psycopg2.DatabaseError) as error:
-                logging.warning(error)
-
     def insert_user(self, user_name: str, user_link: str, total_karma: int, comment_karma: int, link_karma: int,
                     cake_day: str) -> None:
-        """Insertion a new user into pg_users table"""
+        """
+        Insertion a new user into pg_users table
+        :param user_name:
+        :param user_link:
+        :param total_karma:
+        :param comment_karma:
+        :param link_karma:
+        :param cake_day:
+        :return:
+        """
         cmd = "INSERT INTO pg_users (user_name, user_link, total_karma, comment_karma, link_karma, cake_day) " \
               "VALUES (%s, %s, %s, %s, %s, %s)"
         is_duplicate = "SELECT count(*) FROM pg_users WHERE user_name LIKE %s"
@@ -97,34 +96,51 @@ class PostgreDB:
                 return
             cursor.execute(cmd, (user_name, user_link, total_karma, comment_karma, link_karma, cake_day))
 
-    def insert_post(self, uid: str, author: str, post_category: str, post_date: str, number_of_comments: int,
-                    post_votes: int, post_link: str) -> None:
-        """Insertion a new post into pg_posts table"""
+    def insert_post(self, uid: str, author: str, post_category: str, post_date: str, number_of_comments: str,
+                    post_votes: str, post_link: str) -> None:
+        """
+        Insertion a new post into pg_posts table
+        :param uid: unique id of post
+        :param author: name of post author
+        :param post_category:
+        :param post_date:
+        :param number_of_comments:
+        :param post_votes:
+        :param post_link:
+        :return: None
+        """
         cmd = "INSERT INTO pg_posts (" \
               "post_uid, author, post_category, post_date, number_of_comments, post_votes, post_link" \
               ") VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        # find the same post_link in db
         is_duplicate = "SELECT count(*) FROM pg_posts WHERE post_link LIKE %s"
+        # find the user_name in db for getting user_id as author
         user_id_is = "SELECT user_id FROM pg_users WHERE user_name LIKE %s"
 
         connection = self.connection_to_db()
         with connection:
             cursor = connection.cursor()
+            # if the same post_link exist in db - logging warning happens
             cursor.execute(is_duplicate, (post_link,))
             count_of_duplicate = cursor.fetchone()
             if count_of_duplicate[0]:
                 logging.warning("duplicates detected")
                 return
             try:
+                # getting user_id as author or logging warning happens
                 cursor.execute(user_id_is, (author,))
                 user_id = cursor.fetchone()[0]
                 cursor.execute(cmd, (uid, user_id, post_category, post_date, number_of_comments, post_votes, post_link))
             except TypeError as te:
                 logging.warning(f'There is no "{author}" user at db')
             except Exception as e:
-                logging.warning(e)
+                logging.warning(f'{e}')
 
     def get_all_entry(self) -> list:
-        """Get all data from postgre DB"""
+        """
+        Get all data from DataBase
+        :return: list of all post`s data
+        """
         create_view = "CREATE OR REPLACE VIEW view_all AS SELECT " \
                       "p.post_uid, p.post_category, p.post_link, p.number_of_comments, p.post_votes, " \
                       "p.post_date, u.user_name, u.total_karma, u.comment_karma, u.cake_day " \
@@ -142,7 +158,11 @@ class PostgreDB:
             return to_list
 
     def get_one_entry(self, uid: str) -> list:
-        """Get certain data from postgre DB"""
+        """
+        Get data of one post from DataBase
+        :param uid: unique id of post
+        :return: list of specific post`s data
+        """
         cmd = "SELECT p.post_uid, p.post_category, p.post_link, p.number_of_comments, p.post_votes, " \
               "p.post_date, u.user_name, u.total_karma, u.comment_karma, u.cake_day " \
               "FROM pg_posts as p JOIN pg_users as u " \
@@ -157,10 +177,13 @@ class PostgreDB:
                 to_list = [f'{elem}' for elem in result]
                 return to_list
             except Exception as e:
-                logging.warning(f'There is no such element in DB - {uid} ' + str(e))
+                logging.warning(f'There is no such element in DB - {uid}: {e}')
 
     def delete_post(self, uid: str) -> None:
-        """Delete entry from postgre DB"""
+        """
+        Delete entry from postgre DB
+        :param uid: unique id of post
+        """
         cmd = "DELETE FROM pg_posts WHERE post_uid LIKE %s"
         connection = self.connection_to_db()
         with connection:
@@ -168,14 +191,23 @@ class PostgreDB:
             try:
                 cursor.execute(cmd, (uid,))
             except Exception as e:
-                logging.warning(f'There is no such element in DB - {uid} ' + str(e))
+                logging.warning(f'There is no such element in DB - {uid} : {e}')
 
-    def update_post(self, uid: str, val: list) -> None:
-        """Update post in pg_posts"""
-        i_gen = (item for item in val)
-        cmd = f"UPDATE pg_posts SET post_uid='{next(i_gen)}', author={next(i_gen)}, post_category='{next(i_gen)}', " \
-              f"post_link='{next(i_gen)}', number_of_comments={next(i_gen)}, " \
-              f"post_votes={next(i_gen)}, post_date='{next(i_gen)}'" \
+    def update_post(self, uid: str, author: str, post_category: str, post_date: str, number_of_comments: str,
+                    post_votes: str, post_link: str) -> None:
+        """
+        Update post in pg_posts
+        :param uid:
+        :param author:
+        :param post_category:
+        :param post_date:
+        :param number_of_comments:
+        :param post_votes:
+        :param post_link:
+        :return:
+        """
+        cmd = f"UPDATE pg_posts SET author={author}, post_category='{post_category}', post_date='{post_date}', " \
+              f"number_of_comments={number_of_comments}, post_votes={post_votes},  post_link='{post_link}'" \
               f"WHERE post_uid LIKE %s"
         connection = self.connection_to_db()
         with connection:
@@ -183,26 +215,36 @@ class PostgreDB:
             try:
                 cursor.execute(cmd, (uid,))
             except Exception as e:
-                logging.warning(f'There is no such post_uid in DB - {uid} ' + str(e))
+                logging.warning(f'There is no such post_uid in DB - {uid}: {e}')
 
-    def update_user(self, user: str, val: list) -> None:
-        """Update user in gp_users"""
-        i_gen = (item for item in val)
-        cmd = f"UPDATE pg_users SET user_name='{next(i_gen)}', total_karma={next(i_gen)}, " \
-              f"comment_karma={next(i_gen)}, cake_day='{next(i_gen)}'" \
+    def update_user(self, old_u_name: str, user_name: str, user_link: str, total_karma: int, comment_karma: int,
+                    cake_day: str) -> None:
+        """
+        Update user in gp_users
+        :param old_u_name: user name searching for update
+        :param user_name: new user_name
+        :param user_link: new user_link
+        :param total_karma: new total_karma
+        :param comment_karma: new comment_karma
+        :param cake_day: new cake_day
+        :return: None
+        """
+
+        cmd = f"UPDATE pg_users SET user_name='{user_name}', user_link='{user_link}',total_karma={total_karma}, " \
+              f"comment_karma={comment_karma}, cake_day='{cake_day}'" \
               f"WHERE user_name LIKE %s"
         connection = self.connection_to_db()
         with connection:
             cursor = connection.cursor()
             try:
-                cursor.execute(cmd, (user,))
+                cursor.execute(cmd, (old_u_name,))
             except Exception as e:
-                logging.warning(f'There is no such user_name in DB - {user} ' + str(e))
+                logging.warning(f'There is no such user_name in DB - {old_u_name}: {e}')
 
-# postgre_db = PostgreDB()
-# postgre_db.create_table_posts()
-# postgre_db.create_table_users()
-# postgre_db.insert_user('second_user', 2000, 20, datetime(2021, 12, 12).strftime('%Y/%m/%d'))
+
+postgre_db = PostgreDB()
+
+postgre_db.insert_user('second_user', 'second_user_link', 2000, 200, 20, datetime(2021, 12, 12).strftime('%Y/%m/%d'))
 # postgre_db.insert_user('first_user', 1001, 11, datetime(2011, 1, 1).strftime('%Y/%m/%d'))
 # postgre_db.insert_user('new_user', 3000, 30, datetime(2020, 11, 11).strftime('%Y/%m/%d'))
 # postgre_db.insert_post("df8aef88", "second_user", "post_category", "post_link1", 22, 33, datetime.now().strftime('%Y/%m/%d'))
